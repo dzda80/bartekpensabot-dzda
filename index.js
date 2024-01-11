1d4f95114735daa0efa9780edc88c9fd12db8637
Davide Zandonella <davide.zandonella@deltatre.com>
2024-01-11 09:05:22 +0100

    domandone

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.onText(/\/start/, (msg) => {
   // Tastiera personalizzata
   const options = {
     reply_markup: JSON.stringify({
-      keyboard: [['Mangiamo?', 'Domandati', 'ics'], ['QuelloDice']],
+      keyboard: [['Mangiamo', 'Domandati', 'ics'], ['QuelloDice']],
       one_time_keyboard: false,
     }),
   };
